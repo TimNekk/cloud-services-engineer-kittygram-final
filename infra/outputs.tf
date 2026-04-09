f0e2c7bb63f6a4b90c72fd2a32e2a029c1404041
Tim <herew26@gmail.com>
2026-04-09 14:47:09 +0300

    ✨ Add username output to terraform

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -7,3 +7,8 @@ output "vm_1_public_ip" {
   description = "Public IP address of created VM"
   value       = yandex_compute_instance.vm_1.network_interface.0.nat_ip_address
 }
+
+output "vm_1_username" {
+  description = "Username of created VM"
+  value       = var.vm_1_user
+}
